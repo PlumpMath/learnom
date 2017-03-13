3548958a0b8a0bcc853ba04f5f6599fa50a3f61a
Thinh P. Tran <trphthinh@gmail.com>
2017-03-13 13:16:11 +0700

    Allow to add contacts.

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -2,7 +2,8 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]))
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -63,6 +64,24 @@
 ;              (dom/span nil (display-name contact))
 ;              (dom/button nil "Delete")))))
 
+;; Adding contacts
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+              (== c 1) (assoc :middle-initial middle)
+              (>= c 2) (assoc :middle middle)))))
+
+(defn add-contact [data owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                        .-value
+                        parse-contact)]
+    (when new-contact
+      (om/transact! data :contacts #(conj % new-contact)))))
+
 ;; This is a error stuff
 (defn contact-view [contact owner]
   (reify
@@ -71,6 +90,11 @@
       (dom/li nil
               (dom/span nil (display-name contact))
               (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
+
+
+;Reminder: Notice that we're using vec to transform the result of remove (a lazy sequence)
+;back into a vector, consistent with our aforementioned rule that state should only consist
+;of associative data structures like maps and vectors.
 
 (defn contacts-view [data owner]
   (reify
@@ -91,7 +115,10 @@
                (dom/h2 nil "Contact list")
                (apply dom/ul nil
                       (om/build-all contact-view (:contacts data)
-                                    {:init-state {:delete delete}}))))))
+                                    {:init-state {:delete delete}}))
+               (dom/div nil
+                        (dom/input #js {:type "text" :ref "new-contact"})
+                        (dom/button #js {:onClick #(add-contact data owner)} "Add contact"))))))
 
 ;(om/root
 ;  (fn [data owner]
@@ -105,7 +132,7 @@
 ;  app-state
 ;  {:target (. js/document (getElementById "contacts"))})
 
-
+;; The main om-root
 (om/root
   (fn [data owner]
     (reify
